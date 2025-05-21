6b3b06c553dafc653d522776b3eb6d8555613d5a
Simon Harris <simon@pearfalse.com>
2025-05-21 21:41:41 +0100

    Use `withUnsafePointer` wrapper in PointerTests to avoid risk of dangling pointers

--- SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunnerTests/PointerTests.swift
+++ SwiftRustIntegrationTestRunner/SwiftRustIntegrationTestRunnerTests/PointerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Foundation
 @testable import SwiftRustIntegrationTestRunner
 
 class PointerTests: XCTestCase {
@@ -18,35 +19,63 @@ class PointerTests: XCTestCase {
     }
 
     func testSwiftCallRustCvoid() throws {
-        let value = [1, 2, 3]
         var u8 = UInt8(4)
         
-        let pointer = UnsafeRawPointer(value)
-        let pointer_mut = UnsafeMutableRawPointer(mutating: value)
-        let pointer_nil: UnsafeRawPointer? = nil
-        let pointer_mut_nil: UnsafeMutableRawPointer? = nil
-        
-        let pointer_copy = rust_echo_const_c_void(pointer)
-        let pointer_nil_copy = rust_echo_const_c_void(pointer_nil)
-        let pointer_mut_copy = rust_echo_mut_c_void(pointer_mut)
-        let pointer_mut_copy2 = rust_echo_optional_non_null_c_void(pointer_mut)
-        let pointer_mut_nil_copy = rust_echo_mut_c_void(pointer_mut_nil)
-        let pointer_mut_nil_copy2 = rust_echo_optional_non_null_c_void(pointer_mut_nil)
-
-        let pointer_non_null = UnsafeMutableRawPointer(&u8)
-        let pointer_non_null_copy = rust_echo_non_null_c_void(pointer_non_null)
-
-        // TODO: we should run the u8 variants here too
-        
-        XCTAssertEqual(pointer, pointer_copy)
-        XCTAssertEqual(pointer_nil, pointer_nil_copy)
-        XCTAssertEqual(pointer_mut, pointer_mut_copy)
-        XCTAssertEqual(pointer_mut, pointer_mut_copy2)
-        XCTAssertEqual(pointer_mut_nil, pointer_mut_nil_copy)
-        XCTAssertEqual(pointer_mut_nil, pointer_mut_nil_copy2)
-        XCTAssertEqual(pointer_non_null, pointer_non_null_copy)
+        withUnsafePointer(to: u8, {(value) in
+            let pointer = UnsafeRawPointer(value)
+            let pointer_mut = UnsafeMutableRawPointer(mutating: value)
+            let pointer_nil: UnsafeRawPointer? = nil
+            let pointer_mut_nil: UnsafeMutableRawPointer? = nil
+            
+            let pointer_copy = rust_echo_const_c_void(pointer)
+            let pointer_nil_copy = rust_echo_const_c_void(pointer_nil)
+            let pointer_mut_copy = rust_echo_mut_c_void(pointer_mut)
+            let pointer_mut_copy2 = rust_echo_optional_non_null_c_void(pointer_mut)
+            let pointer_mut_nil_copy = rust_echo_mut_c_void(pointer_mut_nil)
+            let pointer_mut_nil_copy2 = rust_echo_optional_non_null_c_void(pointer_mut_nil)
+            
+            let pointer_non_null = UnsafeMutableRawPointer(mutating: value)
+            let pointer_non_null_copy = rust_echo_non_null_c_void(pointer_non_null)
+            
+            XCTAssertEqual(pointer, pointer_copy)
+            XCTAssertEqual(pointer_nil, pointer_nil_copy)
+            XCTAssertEqual(pointer_mut, pointer_mut_copy)
+            XCTAssertEqual(pointer_mut, pointer_mut_copy2)
+            XCTAssertEqual(pointer_mut_nil, pointer_mut_nil_copy)
+            XCTAssertEqual(pointer_mut_nil, pointer_mut_nil_copy2)
+            XCTAssertEqual(pointer_non_null, pointer_non_null_copy)
+        })
     }
-    
+
+    func testSwiftCallRustUInt8() throws {
+        var u8 = UInt8(4)
+
+        withUnsafePointer(to: u8, {(value) in
+            let pointer = UnsafePointer<UInt8>(value)
+            let pointer_mut = UnsafeMutablePointer<UInt8>(mutating: value)
+            let pointer_nil: UnsafePointer<UInt8>? = nil
+            let pointer_mut_nil: UnsafeMutablePointer<UInt8>? = nil
+            
+            let pointer_copy = rust_echo_const_u8(pointer)
+            let pointer_nil_copy = rust_echo_const_u8(pointer_nil)
+            let pointer_mut_copy = rust_echo_mut_u8(pointer_mut)
+            let pointer_mut_copy2 = rust_echo_optional_non_null_u8(pointer_mut)
+            let pointer_mut_nil_copy = rust_echo_mut_u8(pointer_mut_nil)
+            let pointer_mut_nil_copy2 = rust_echo_optional_non_null_u8(pointer_mut_nil)
+            
+            let pointer_non_null = UnsafeMutablePointer<UInt8>(&u8)
+            let pointer_non_null_copy = rust_echo_non_null_u8(pointer_non_null)
+            
+            XCTAssertEqual(pointer, pointer_copy)
+            XCTAssertEqual(pointer_nil, pointer_nil_copy)
+            XCTAssertEqual(pointer_mut, pointer_mut_copy)
+            XCTAssertEqual(pointer_mut, pointer_mut_copy2)
+            XCTAssertEqual(pointer_mut_nil, pointer_mut_nil_copy)
+            XCTAssertEqual(pointer_mut_nil, pointer_mut_nil_copy2)
+            XCTAssertEqual(pointer_non_null, pointer_non_null_copy)
+        })
+    }
+
     func testRustCallSwiftCvoid() throws {
         rust_run_opaque_pointer_tests()
     }
